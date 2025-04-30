8debff2a9188cde43e7de37f6f8887581909793a
chenzhenyang <andy271828@163.com>
2025-04-30 23:59:54 +0800

    完成shell脚本的wc完整流程

--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
-从零开始基于Java重构2020的分布式系统课程
+从零开始基于Java重构2020的mit6.824分布式系统课程lab
+
+原课程链接：http://nil.csail.mit.edu/6.824/2020/schedule.html
--- src/main/java/cn/andychen/Constants.java
+++ src/main/java/cn/andychen/Constants.java
@@ -11,9 +11,11 @@ import java.nio.file.Paths;
  */
 public class Constants {
     public static final String ROOT_PATH = "D:/Projects/distributed-system/src/main/java/cn/andychen/";
+    // 文件读取绝对路径，方便脚本调用
     public static final String DATA_PATH = "D:/Projects/distributed-system/data/";
     public static final String SEP = " ";
     public static final String EOL = "\n";
+    // mapReduce具体任务类别
     public class AppType{
         public static final String AppType_WC = "word_counter";
         public static final String AppType_INDEXER = "indexer";
